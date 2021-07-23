9aeb279fc5bf366e495e11c4f2a112f19853b327
Dakouri M. Kobri <maurille.kobri@gmail.com>
2021-07-23 09:42:42 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Products from "./Products";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./Detail";
+import Cart from "./Cart";
+
+export default function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(id, sku) {
+    setCart((items) => {
+      const itemInCart = items.find((i) => i.sku === sku);
+      if (itemInCart) {
+        // Return a new array with the matching item replaced
+        return items.map((i) =>
+          i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i
+        );
+      } else {
+        // Return a new array with the matching item appended
+        return [...items, { id, sku, quantity: 1 }];
+      }
+    });
+  }
+
+  return (
+    <>
+      <div className="content">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
+            <Route path="/:category" element={<Products />} />
+            <Route
+              path="/:category/:id"
+              element={<Detail addToCart={addToCart} />}
+            />
+            <Route path="/cart" element={<Cart cart={cart} />} />
+          </Routes>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
